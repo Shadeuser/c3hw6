96fcc1e8071234b3e82df6cafd2dc87b7dccea7e
Shade <lexxus80@yandex.ru>
2020-07-06 15:09:08 +0500

    pull

--- src/main/java/lesson6/MyTasks.java
+++ src/main/java/lesson6/MyTasks.java
@@ -1,5 +1,6 @@
 package lesson6;
 
+
 public class MyTasks {
 
     public int[] doArrayFirst(int[] ar) {
--- src/test/java/HwTest2.java
+++ src/test/java/HwTest2.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collection;
